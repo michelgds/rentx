a8ed46226ea2f193cec4d4e112cc7d7981e9bc56
Michel G. dos Santos <michel.dossantos@worldpay.com>
2022-03-04 12:41:48 -0300

    Create id using uuid

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,20 @@
-import { Router, response } from 'express';
-import { v4: uuidV4 } from 'uuid';
+import { Router } from 'express';
+import { v4 as uuidV4 } from 'uuid';
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({
+  const category = {
+    id: uuidV4(),
     name,
     description
-  });
+  }
+
+  categories.push(category);
 
   return response.status(201).send();
 });
