a6b2a066945c98f01bcbce0562670d6818308d0f
Michel G. dos Santos <michel.dossantos@worldpay.com>
2022-03-07 12:49:53 -0300

    Map the Categories entity

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,9 +1,18 @@
 import { v4 as uuidV4 } from 'uuid';
+import { Entity, PrimaryColumn, Column, CreateDateColumn } from "typeorm";
 
+@Entity("categories")
 class Category {
+  @PrimaryColumn()
   id?: String;
+
+  @Column()
   name: String;
+
+  @Column()
   description: String;
+
+  @CreateDateColumn()
   createdAt: Date;
 
   constructor() {
