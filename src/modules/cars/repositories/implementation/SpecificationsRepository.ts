2f75bdd261e59897168333782f10734309d6834c
Michel G. dos Santos <michel.dossantos@worldpay.com>
2022-03-07 12:45:33 -0300

    Rename model folder to entities

--- src/modules/cars/repositories/implementation/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementation/SpecificationsRepository.ts
@@ -1,5 +1,5 @@
 
-import { Specification } from "../../model/Specification";
+import { Specification } from "../../entities/Specification";
 import { ISpecificationRepository, ICreateSpecificationDTO } from "../ISpecificationsRepository";
 
 
--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,5 +1,5 @@
 import { ISpecificationRepository } from "../../repositories/ISpecificationsRepository";
-import { Specification } from "../../model/Specification";
+import { Specification } from "../../entities/Specification";
 
 interface IRequest {
   name: String;
