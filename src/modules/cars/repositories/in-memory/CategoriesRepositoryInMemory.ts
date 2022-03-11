36d13ead618e2fecc6d20a95f68140bec324f51f
Michel G. dos Santos <michel.dossantos@worldpay.com>
2022-03-11 10:47:38 -0300

    Implement tests for CreateCategoryUseCase

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,29 @@
+import { ICategoriesRepository } from "../ICategoriesRepository";
+import { Category } from "../../entities/Category";
+
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+
+  private categories: Category[] = [];
+
+  async findByName(name: string): Promise<Category> {
+    const category = this.categories.find((category) => category.name === name);
+    return category;
+  }
+
+  async list(): Promise<Category[]> {
+    const categories = this.categories;
+    return categories;
+  }
+
+  async create({ name, description }: import("../ICategoriesRepository").ICreateCategoryDTO): Promise<void> {
+    const category = new Category();
+
+    Object.assign(category, {
+      name, description
+    });
+
+    this.categories.push(category);
+  }
+}
+
+export { CategoriesRepositoryInMemory }
--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spect.ts
+++
@@ -1,4 +0,0 @@
-describe("Criar categoria" () => {
-  
-  it("");
-});
