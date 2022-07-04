4035a519e4b45ca0d688a9d3093fcc3dd8988ab7
Michel Gracia dos Santos <michelgraciadossantos@MacBook-Pro-de-Michel.local>
2022-07-04 16:56:19 -0300

    Add ensure admin middleware

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
 import { ListSpecificationsController } from '@modules/cars/useCases/listSpecifications/ListSpecificationsController';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+import { ensureAdmin } from '../middlewares/ensureIsAdmin';
 
 
 const specificationsRoutes = Router();
@@ -10,9 +11,7 @@ const specificationsRoutes = Router();
 const createSpecificationController = new CreateSpecificationController();
 const listSpecificationController = new ListSpecificationsController();
 
-specificationsRoutes.use(ensureAuthenticated);
-
-specificationsRoutes.post("/", createSpecificationController.handle);
+specificationsRoutes.post("/", ensureAuthenticated, ensureAdmin, createSpecificationController.handle);
 
 specificationsRoutes.get("/", listSpecificationController.handle);
 
