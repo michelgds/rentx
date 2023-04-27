170fade44d09c6be89e3e6571e14466b761cdd94
Michel Gracia <michel.gracia@thoughtworks.com>
2023-04-26 22:29:30 -0300

    Add await for save in repository

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -3,37 +3,37 @@ import { Rental } from "../entities/Rental";
 import { IRentalsRepository } from "../../../repositories/IRentalsRepository";
 
 class RentalsRepository implements IRentalsRepository {
-    private repository: Repository<Rental>;
+  private repository: Repository<Rental>;
 
-    constructor() {
-        this.repository = getRepository(Rental);
-    }
+  constructor() {
+    this.repository = getRepository(Rental);
+  }
 
-    async findById(id: string): Promise<Rental> {
-        return await this.repository.findOne(id);
-    }
+  async findById(id: string): Promise<Rental> {
+    return await this.repository.findOne(id);
+  }
 
-    async findOpenRentalByCar(car_id: string): Promise<Rental> {
-        return await this.repository.findOne({car_id});
-    }
-    async findOpenRentalByUser(user_id: string): Promise<Rental> {
-        return await this.repository.findOne({user_id});
-    }
-    async create({
-        car_id, 
-        user_id, 
-        expected_return_date
-    }: ICreateRentalDTO): Promise<Rental> {
-        const rental = this.repository.create({
-            car_id,
-            user_id,
-            expected_return_date
-        });
+  async findOpenRentalByCar(car_id: string): Promise<Rental> {
+    return await this.repository.findOne({ car_id });
+  }
+  async findOpenRentalByUser(user_id: string): Promise<Rental> {
+    return await this.repository.findOne({ user_id });
+  }
+  async create({
+    car_id,
+    user_id,
+    expected_return_date,
+  }: ICreateRentalDTO): Promise<Rental> {
+    const rental = this.repository.create({
+      car_id,
+      user_id,
+      expected_return_date,
+    });
 
-        this.repository.save(rental);
+    await this.repository.save(rental);
 
-        return rental;
-    }
+    return rental;
+  }
 }
 
-export { RentalsRepository }
+export { RentalsRepository };
