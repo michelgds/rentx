1a2447791687bb19ce5d6b60995bd9987af1617b
Michel Gracia <michel.gds@hotmail.com>
2023-08-17 22:49:18 -0300

    Remove findById

--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -4,7 +4,6 @@ interface IRentalsRepository {
     findOpenRentalByCar(car_id: string): Promise<Rental>;
     findOpenRentalByUser(user_id: string): Promise<Rental>;
     create(data: ICreateRentalDTO): Promise<Rental>;
-    findById(id: string): Promise<Rental>;
 }
 
 export { IRentalsRepository }
